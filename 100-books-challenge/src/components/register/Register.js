033c93669c529cab0bd6a80630ebbcd7e9d43f50
Marina Hristova <marina.ts.hristova@gmail.com>
2022-08-07 19:12:53 +0300

    Add  error texts to register form

--- 100-books-challenge/src/components/register/Register.js
+++ 100-books-challenge/src/components/register/Register.js
@@ -9,17 +9,26 @@ export const Register = () => {
                 <form>
                     <label for="firstname"> First Name:
                         <input type="text" name="firstname" placeholder="Ivan" />
+                        <p className='error-text'> First name should be at least 3 characters long!</p>
                     </label>
                     <label for="lastname">Last Name:
                         <input type="text" name="lastname" placeholder="Ivanov" />
+                        <p className='error-text'> Last name should be at least 3 characters long!</p>
+
                     </label>
                     <label for="email">Email:
                         <input type="text" name="emai" placeholder="example@mail.com" />
+                        <p className='error-text'>Email is not valid!</p>
                     </label>
                     <label for="password"> Password:
                         <input type="password" name="password" placeholder="******" />
+                        <p className='error-text'> Password should be at least 6 characters long!</p>
                     </label>
-                  
+                    <label for="confirm-password"> Confirm Password:
+                        <input type="password" name="confirm-password" placeholder="******" />
+                        <p className='error-text'> The password confirmation does not match!</p>
+                    </label>
+
                     <button className='register-button'>Register</button>
 
                 </form>
