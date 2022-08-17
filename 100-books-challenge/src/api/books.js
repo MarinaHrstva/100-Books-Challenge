1944b4dd1d42bd08539cf73f2f3a579e294ebab0
Marina Hristova <marina.ts.hristova@gmail.com>
2022-08-17 10:52:33 +0300

    Add API comments.js and likes.js

--- 100-books-challenge/src/api/books.js
+++ 100-books-challenge/src/api/books.js
@@ -28,11 +28,5 @@ export async function likeBook(bookId) {
     return post('/data/likes', { bookId });
 }
 
-export async function getAllLikes(bookId) {
-    return get(`/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`)
-}
 
-export async function getLikesByUserId(bookId, userId) {
-    return get(`/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`)
-}
 
---
+++ 100-books-challenge/src/api/likes.js
@@ -0,0 +1,7 @@
+export async function getAllLikes(bookId) {
+    return get(`/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`)
+}
+
+export async function getLikesByUserId(bookId, userId) {
+    return get(`/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`)
+}
