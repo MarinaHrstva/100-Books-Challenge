12b6209d9351c1922a5780f2022801d292782a97
Marina Hristova <marina.ts.hristova@gmail.com>
2022-08-19 09:54:15 +0300

    Fixed user nav style and user hero-section button route

--- 100-books-challenge/src/components/hero-section/HeroSection.js
+++ 100-books-challenge/src/components/hero-section/HeroSection.js
@@ -1,14 +1,18 @@
 
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../contexts/UserContext'
 
 import './HeroSection.css'
 
 export const HeroSection = () => {
+const {user}=useContext(UserContext);
+
     return (
      <div className='hero-section-container'>
         <section className="hero-section">
             <h1>100 Books Challenge</h1>
-            <Link to='register'>
+            <Link to={user==''?'/register': '/create'}>
             <button className='button-primary'>Start the Challenge</button>
             </Link>
             <p>The 100 Books Everyone Should Read</p>
